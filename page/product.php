4cc3af3ec984a7c351afe0814de60c7e49711f5f
Thangpn <thangpn.mc@gmail.com>
2019-02-12 04:25:07 +0700

    up

---
+++ page/product.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>3</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="" />
+	<link type="text/css" rel="stylesheet" href="../css/style.css" />
+	<link rel="stylesheet" href="../font/css/font-awesome.min.css">
+
+</head>
+
+<body>
+	<div class="wrapper">
+		<?php require('../layout/header.php') ?>
+		<div class="main-content">
+		<?php require('../layout/header_top.php') ?>
+		<?php require('../layout/header_menu.php') ?>
+        <?php require('../layout/navigation.php') ?>
+        <div class="container detail-product">
+            <div class="detail-product__review-content">
+                <div class="box-left">
+                    <div class="box-left__view-img">
+						<div class="icon">
+							<i class="fa fa-angle-up" aria-hidden="true"></i>
+							</div>
+							<img src="../img/img_zoom1.jpg" alt="">
+							<img src="../img/img_zoom1.jpg" alt="">
+							<img src="../img/img_zoom1.jpg" alt="">
+							<img src="../img/img_zoom1.jpg" alt="">
+							<div class="icon">
+							<i class="fa fa-angle-down" aria-hidden="true"></i>
+							</div>
+							<div class="seach">
+							<i class="fa fa-search-plus" aria-hidden="true"></i>
+							</div>
+						
+					</div>
+					<div class="box-left__zoom-img">
+						 <img src="../img/product0.jpg" alt="">
+					</div>
+                </div> 
+                <div class="box-right">
+					<div class="box-right__panigation-top">
+						<p>
+							<i class="fa fa-angle-left" aria-hidden="true"></i>
+							<span>2 OF 197</span>
+							<i class="fa fa-angle-right" aria-hidden="true"></i>
+						</p>
+					</div>
+					<div class="box-right__title">
+						<div class="code">
+							<p>MU-4587-89</p>
+						
+						</div>
+						<h3>DETAILED SWING DRESS</h3>
+					</div>
+					<div class="box-right__review">
+						<div class="star">
+							<i class="fa fa-star" aria-hidden="true"></i>
+							<i class="fa fa-star" aria-hidden="true"></i>
+							<i class="fa fa-star" aria-hidden="true"></i>
+							<i class="fa fa-star" aria-hidden="true"></i>
+							<i class="fa fa-star" aria-hidden="true"></i>
+							<div class="text">
+								<p>19 Review(s)</p>
+								<p>Add Review</p>
+							</div>
+						</div>
+						<div class="availabilty">
+							<p>Availabilty: <span>In stock</span></p>
+							<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod excepturi adipisci cum</p>
+						</div>
+					</div>
+					<div class="box-right__total">
+						<p><i class="fa fa-usd" aria-hidden="true"></i>1,875.00</p>
+					</div>
+					<div class="box-right__detail-view">
+						<div class="size item-toggle">
+							<p>SIZE</p>
+							<p><i class="fa fa-angle-down" aria-hidden="true"></i></p>
+						</div>
+						<div class="color item-toggle">
+							<p>COLOR</p>
+							<p><i class="fa fa-angle-down" aria-hidden="true"></i></p>
+						</div>
+						<div class="q-ty item-toggle">
+							<p>QTY</p>
+							<p><i class="fa fa-angle-down" aria-hidden="true"></i></p>
+						</div>
+						<div class="button-product">
+							<div class="button add-cart">
+								<a href="#">ADD TO CART </a>
+							</div>
+							<div class="button wishlist">
+								<a href="#">WISHLIST </a>
+							</div>
+						</div>
+					</div>
+					<div class="box-right__introduce">
+					<div class="description">
+						<div class="title">
+							<p>description</p>
+							<p><i class="fa fa-caret-down" aria-hidden="true"></i></p>
+						</div>
+						<div class="des-conten">
+							<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur, a necessitatibus. Minima provident cumque eum quis, odit alias, voluptatibus quasi deserunt debitis, necessitatibus aspernatur ullam ab facilis unde amet rem!</p>
+						</div>
+					</div>
+					<div class="additionnal">
+						<div class="title">
+							<p>ADDITIONNAL INFO</p>
+							<p><i class="fa fa-caret-down" aria-hidden="true"></i></p>
+						</div>
+					</div>
+					<div class="tags">
+						<div class="title">
+							<p>TAGS</p>
+							<p><i class="fa fa-caret-down" aria-hidden="true"></i></p>
+						</div>
+					</div>
+					<div class="share">
+						<div class="title">
+							<p>SHARE</p>
+						</div>
+						<div class="icon">
+						<i class="fa fa-facebook" aria-hidden="true"></i>
+						<i class="fa fa-twitter" aria-hidden="true"></i>
+						<i class="fa fa-twitter" aria-hidden="true"></i>
+						<i class="fa fa-twitter" aria-hidden="true"></i>
+						<i class="fa fa-twitter" aria-hidden="true"></i>
+						</div>
+
+					</div>
+					</div>
+				</div> 
+            </div>
+            <div class="detail-product__related">
+				<div class="box-left">
+					<div class="box-left__title">
+						<hr class="hr-text" data-content="RELATED PRODUCTS">
+						<div class="icon">
+							<i class="fa fa-angle-left" aria-hidden="true"></i>
+							<i class="fa fa-angle-right" aria-hidden="true"></i>
+						</div>
+					</div>
+					<div class="box-left__list-item">
+						<div class="item">
+							<div class="avatar">
+							<img src="../img/product1.jpg" alt="">
+							</div>
+							<h4>DETAILED SWING DRESS</h4>
+							<p><i class="fa fa-usd" aria-hidden="true"></i>1,875.00</p>
+							
+						</div>
+						<div class="item">
+							<div class="avatar">
+							<img src="../img/product1.jpg" alt="">
+							</div>
+							<h4>DETAILED SWING DRESS</h4>
+							<p><i class="fa fa-usd" aria-hidden="true"></i>1,875.00</p>
+							
+						</div>
+						<div class="item">
+							<div class="avatar">
+							<img src="../img/product1.jpg" alt="">
+							</div>
+							<h4>DETAILED SWING DRESS</h4>
+							<p><i class="fa fa-usd" aria-hidden="true"></i>1,875.00</p>
+							
+						</div>
+						<div class="item">
+							<div class="avatar">
+							<img src="../img/product1.jpg" alt="">
+							</div>
+							<h4>DETAILED SWING DRESS</h4>
+							<p><i class="fa fa-usd" aria-hidden="true"></i>1,875.00</p>
+							
+						</div>
+					</div>
+				</div>
+				<div class="box-right">
+					<div class="box-right__reviews">
+						<div class="title-top">
+							<h3>19 REVEIWS</h3>
+						</div>
+						<div class="list-evaluate">
+							<div class="list-evaluate__item">
+								<div class="title">
+									<p>MUST HAVE</p>
+									<div class="icon-star">
+									<i class="fa fa-star" aria-hidden="true"></i>
+									<i class="fa fa-star" aria-hidden="true"></i>
+									<i class="fa fa-star" aria-hidden="true"></i>
+									<i class="fa fa-star" aria-hidden="true"></i>
+									<i class="fa fa-star" aria-hidden="true"></i>
+									</div>
+								</div>
+								<div class="content">
+									<p>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									</p>
+								</div>
+								<div class="detail-user">
+									<div class="date">
+										<p>Feb 09,2014</p>
+									</div>
+									<div class="name"> <p> Jane Moren</p></div>
+								</div>
+							</div>
+							<div class="list-evaluate__item">
+								<div class="title">
+									<p>MUST HAVE</p>
+									<div class="icon-star">
+									<i class="fa fa-star" aria-hidden="true"></i>
+									<i class="fa fa-star" aria-hidden="true"></i>
+									<i class="fa fa-star" aria-hidden="true"></i>
+									<i class="fa fa-star" aria-hidden="true"></i>
+									<i class="fa fa-star" aria-hidden="true"></i>
+									</div>
+								</div>
+								<div class="content">
+									<p>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									</p>
+								</div>
+								<div class="detail-user">
+									<div class="date">
+										<p>Feb 09,2014</p>
+									</div>
+									<div class="name"><p> Jane Moren</p></div>
+								</div>
+							</div>
+						</div>
+						<div class="button-review">
+							<a href="#">WRITE REIEW</a>
+						</div>
+
+					</div>
+				</div>
+			</div>
+        </div>
+
+
+        
+        <?php require('../layout/top_brands.php') ?>
+		<?php require('../layout/footer.php') ?>
+		</div>
+	</div>
+</body>
+
+</html>
