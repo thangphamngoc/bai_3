7faf0b5019a53ac9f81b6b63d76889cbce93c88d
Thangpn <thangpn.mc@gmail.com>
2019-02-22 02:45:33 +0700

    up

--- layout/sidebar_mb.php
+++ layout/sidebar_mb.php
@@ -1,15 +1,15 @@
 <div class="sidebar-wrapper-mb" id="js-my-top">
 					<ul id="js-my-side" class="sidenav">
 						
-						    <li class=""><a href="#">Login</a></li>
-					 		<li class=""><a href="#">WOMEN</a></li>
+						    <li class=""><a href="http://localhost/page/login.php">Login</a></li>
+					 		<li class=""><a href="http://localhost/page/category.php">WOMEN</a></li>
 					 		<li class=""><a href="#">MEN</a></li>
 					 		<li class=""><a href="#">KID</a></li>
 					 		<li class=""><a href="#">ACCESORIES</a></li>
 					 		<li class=""><a href="#">WHAT NEWS</a></li>
 					 		<li class=""><a href="#">BRANDS</a></li>
 					 		<li class=""><a href="#">SALE</a></li>
-					 		<li class=""><a href="#">BLOG</a></li>
+					 		<li class=""><a href="http://localhost/page/blog.php">BLOG</a></li>
 					</div>
 					</ul>
 		</div>
